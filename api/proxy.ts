ee172e4ce25abcaa5c31280863d0e0933b91ca35
wangxiang <1827945911@qq.com>
2023-01-21 22:27:44 +0800

    🧑‍💻🐳 vercel serverless

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,8 +1,21 @@
+const { loadEnv } = require('vite')
+const { wrapperEnv } = require ('../build/utils')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
+  const root = process.cwd();
 
+  // 在当前工作目录中根据 mode 加载env文件
+  const env = loadEnv('build', root);
+
+  // 解析转换环境变量值
+  const viteEnv = wrapperEnv(env);
+  const { VITE_PROXY } = viteEnv;
+
+  console.log(VITE_PROXY)
+  console.log(process.env)
   for (const [prefix, target] of process.env.VITE_PROXY || []) {
+    console.log(1111)
     // 创建代理对象并转发请求
     createProxyMiddleware(prefix, {
       target,
